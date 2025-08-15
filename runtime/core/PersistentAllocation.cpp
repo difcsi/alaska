b6882e9a6d14785584433a81dc979a33a802b891
Nick Wanninger <nickwanninger@gmail.com>
2025-08-15 14:39:46 -0500

    Update the persistent allocation system

--- runtime/core/PersistentAllocation.cpp
+++ runtime/core/PersistentAllocation.cpp
@@ -4,14 +4,14 @@
 #include <alaska/LazyGlobal.hpp>
 
 namespace alaska {
-  LazyGlobal<Arena> persistent_allocation_arena;
+  static LazyGlobal<Arena> persistent_allocation_arena;
 
   void *PersistentAllocation::operator new(size_t size) {
     return persistent_allocation_arena->push(size);
   }
 
   void PersistentAllocation::operator delete(void *ptr) {
-    alaska::printf("Invalid call to delete made for persistent allocation object at %p\n", ptr);
+    // alaska::printf("Invalid call to delete made for persistent allocation object at %p\n", ptr);
   }
 
 
