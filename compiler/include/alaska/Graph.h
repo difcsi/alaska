difcsi/alaska

commit 2431161c9262a481654a039acbb79f0c49c7d2eb

add the first cut of an "intent pass"

compiler/lib/TranslationForest.cpp
@@ -41,7 +41,9 @@ struct TranslationVisitor : public llvm::InstVisitor<TranslationVisitor> {
   void visitGetElementPtrInst(llvm::GetElementPtrInst &I) {
     // Simply insert an `alaska.derive` function right after `I`
     auto t = get_incoming_translated_value(node);
-    node.translated = alaska::insertDerivedBefore(I.getNextNode(), t, &I);
+    I.setOperand(0, t);
+    node.translated = &I;
+    // node.translated = alaska::insertDerivedBefore(I.getNextNode(), t, &I);
   }
 
   void visitLoadInst(llvm::LoadInst &I) {

compiler/passes/Alaska.cpp
@@ -193,6 +193,7 @@ extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginIn
           REGISTER("alaska-replace", AlaskaReplacementPass);
           if (name == "alaska-translate") {
             MPM.addPass(AlaskaTranslatePass(true));
+            MPM.addPass(AlaskaIntentPass());
             return true;
           }
 

compiler/passes/Escape.cpp
@@ -360,13 +360,13 @@ llvm::PreservedAnalyses AlaskaEscapePass::run(llvm::Module &M, llvm::ModuleAnaly
 
 
           if (auto func = dyn_cast<llvm::Function>(call->getCalledOperand())) {
-            llvm::errs() << "escape to argument " << i << " at call to \e[31m\"" << func->getName() << "\e[0m";
-            if (llvm::DILocation *Loc = I.getDebugLoc()) {
-              unsigned Line = Loc->getLine();
-              unsigned Column = Loc->getColumn();  // Optional: Get column number
-              llvm::errs() << " Line: " << Line << ", Column: " << Column << "\n";
-            }
-            llvm::errs() << "\n";
+            // llvm::errs() << "escape to argument " << i << " at call to \e[31m\"" <<
+            // func->getName() << "\e[0m"; if (llvm::DILocation *Loc = I.getDebugLoc()) {
+            //   unsigned Line = Loc->getLine();
+            //   unsigned Column = Loc->getColumn();  // Optional: Get column number
+            //   llvm::errs() << " Line: " << Line << ", Column: " << Column << "\n";
+            // }
+            // llvm::errs() << "\n";
           }
 
 

compiler/passes/Lower.cpp
@@ -36,7 +36,6 @@ std::vector<llvm::CallBase *> collectCalls(llvm::Module &M, const char *name) {
 
 bool collectOffsets(GetElementPtrInst *gep, const DataLayout &DL, unsigned BitWidth,
     std::unordered_map<Value *, APInt> &VariableOffsets, APInt &ConstantOffset) {
-
   auto CollectConstantOffset = [&](APInt Index, uint64_t Size) {
     Index = Index.sextOrTrunc(BitWidth);
     APInt IndexedSize = APInt(BitWidth, Size);
@@ -128,7 +127,7 @@ llvm::PreservedAnalyses AlaskaLowerPass::run(llvm::Module &M, llvm::ModuleAnalys
 
     for (auto *call : collectCalls(M, "alaska.translate")) {
       IRBuilder<> b(call);  // insert after the call
-
+      
       // If the return value is only used in calls, run the more expensive "escape" variant
       bool onlyCalls = true;
       for (auto *user : call->users()) {
@@ -156,10 +155,10 @@ llvm::PreservedAnalyses AlaskaLowerPass::run(llvm::Module &M, llvm::ModuleAnalys
 
   // Lower alaska.derive
   for (auto call : collectCalls(M, "alaska.derive")) {
-    // Release is just a marker for liveness. We don't need (or want) it to be in the
-    // application at the end
+    // Derive is just a marker to indicate that a GEP happened on a translated value.
+    // In the end, we just replace it's uses with the GEP.
+    
     IRBuilder<> b(call);  // insert after the call
-                          //
     auto base = call->getArgOperand(0);
     auto offset = dyn_cast<llvm::GetElementPtrInst>(call->getArgOperand(1));
 

compiler/passes/PinTracking.cpp
@@ -233,7 +233,7 @@ PreservedAnalyses PinTrackingPass::run(Module &M, ModuleAnalysisManager &AM) {
 
       auto called = call->getCalledOperand();
       int patch_size = 0;
-      int id = 'U';
+      char id = 'U';
 
       if (auto func = call->getCalledFunction()) {
         if (func->getName() == "alaska_barrier_poll") {
@@ -245,8 +245,11 @@ PreservedAnalyses PinTrackingPass::run(Module &M, ModuleAnalysisManager &AM) {
         } else if (func->getName() == "alaska_do_handle_fault_check") {
           id = 'H';
           patch_size = ALASKA_PATCH_SIZE;  // TODO: handle ARM
+        } else {
+          id = 'C';
         }
       }
+      alaska::println(id, "  ", *call, "  | ", patch_size);
 
 
 
@@ -313,7 +316,7 @@ PreservedAnalyses PinTrackingPass::run(Module &M, ModuleAnalysisManager &AM) {
 
 
 
-      alaska::println((char)id, ": ", *call);
+      // alaska::println((char)id, ": ", *call);
 
       // If the safepoint is a PATCH safepoint, it won't ever be a call. It will always be a "NOP"
       // style instruction and will never corrupt any registers. As such, to reduce the overhead of

compiler/passes/TranslatePass.cpp
@@ -57,10 +57,11 @@ PreservedAnalyses AlaskaTranslatePass::run(Module &M, ModuleAnalysisManager &AM)
       continue;
     }
 
+
     if (hoist) {
       alaska::insertHoistedTranslations(F);
     } else {
-      alaska::println("Not hoisting in ", F.getName());
+      // alaska::println("Not hoisting in ", F.getName());
       alaska::insertConservativeTranslations(F);
     }
 

compiler/CMakeLists.txt
@@ -72,6 +72,7 @@ add_llvm_library(Alaska MODULE
 	passes/Lower.cpp
 	passes/PlaceSafepoints.cpp
   passes/ArgumentTrace.cpp
+  passes/IntentPass.cpp
 
 	PLUGIN_TOOL
 	opt

compiler/include/alaska/Passes.h
@@ -25,6 +25,13 @@ class AlaskaTranslatePass : public llvm::PassInfoMixin<AlaskaTranslatePass> {
   llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &AM);
 };
 
+class AlaskaIntentPass : public llvm::PassInfoMixin<AlaskaIntentPass> {
+ public:
+   AlaskaIntentPass() {}
+
+  llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &AM);
+};
+
 
 /**
  * PinTrackingPass - Insert pin roots on the stack so the runtime knows where

commit aad66dee86938e805e34bbf6856dadc6478fe2c6

update the compiler to llvm 21
This update also fixes a compiler bug introduced in TranslationForest
from the commit 2431161c9262a481654a039acbb79f0c49c7d2eb

CMakeLists.txt
@@ -121,7 +121,7 @@ add_subdirectory(runtime)
 
 if(ALASKA_ENABLE_COMPILER)
   # configure LLVM
-  find_package(LLVM 16 REQUIRED CONFIG)
+  find_package(LLVM 21.1.0 REQUIRED CONFIG)
 
   set(LLVM_RUNTIME_OUTPUT_INTDIR ${CMAKE_BINARY_DIR}/${CMAKE_CFG_INTDIR}/)
   set(LLVM_LIBRARY_OUTPUT_INTDIR ${CMAKE_BINARY_DIR}/${CMAKE_CFG_INTDIR}/)

compiler/include/alaska/Graph.h
@@ -277,12 +277,12 @@ namespace alaska {
       auto count(const NodeType &n) { return this->_filter.count(n); }
 
       auto outgoing(const NodeType &n) {
-        auto [begin, end] = this->out_neighbors(n);
+        auto [begin, end] = _graph->out_neighbors(n);
         return filtered_iter_wrapper<decltype(begin)>(begin, end, *this);
       }
 
       auto incoming(const NodeType &n) {
-        auto [begin, end] = this->in_neighbors(n);
+        auto [begin, end] = _graph->in_neighbors(n);
         return filtered_iter_wrapper<decltype(begin)>(begin, end, *this);
       }
 

compiler/include/alaska/graph_lite.h
@@ -460,7 +460,7 @@ namespace graph_lite::detail {
         template<typename U, typename V>
         EdgePropType& edge_prop(U&& source_iv, V&& target_iv) {
             return const_cast<EdgePropType&>(
-                    static_cast<const EdgePropGraphBase*>(this)->template edge_prop(
+                    static_cast<const EdgePropGraphBase*>(this)->edge_prop(
                         std::forward<U>(source_iv),
                         std::forward<V>(target_iv)
                     )

compiler/include/noelle/core/Assumptions.hpp
@@ -63,7 +63,7 @@ namespace llvm {
       bool operator()(const Remedy_ptr &lhs, const Remedy_ptr &rhs) const {
         // if same remedy type use custom comparator,
         // otherwise compare based on cost or remedy name (if cost the same)
-        if (lhs->getRemedyName().equals(rhs->getRemedyName()))
+        if (lhs->getRemedyName() == (rhs->getRemedyName()))
           return lhs->compare(rhs);
         else if (lhs->cost == rhs->cost)
           return (lhs->getRemedyName().compare(rhs->getRemedyName()) == -1);

compiler/lib/TranslationForest.cpp
@@ -41,9 +41,7 @@ struct TranslationVisitor : public llvm::InstVisitor<TranslationVisitor> {
   void visitGetElementPtrInst(llvm::GetElementPtrInst &I) {
     // Simply insert an `alaska.derive` function right after `I`
     auto t = get_incoming_translated_value(node);
-    I.setOperand(0, t);
-    node.translated = &I;
-    // node.translated = alaska::insertDerivedBefore(I.getNextNode(), t, &I);
+    node.translated = alaska::insertDerivedBefore(I.getNextNode(), t, &I);
   }
 
   void visitLoadInst(llvm::LoadInst &I) {
@@ -136,7 +134,7 @@ static llvm::Instruction *compute_translation_insertion_location(
 
 
   if (auto invokeInst = dyn_cast<llvm::InvokeInst>(pointerToTranslate)) {
-    effectivePointerInstruction = invokeInst->getNormalDest()->getFirstNonPHIOrDbg();
+    effectivePointerInstruction = &*invokeInst->getNormalDest()->getFirstNonPHIOrDbg();
   } else if (auto pointerToTranslateInst = dyn_cast<llvm::Instruction>(pointerToTranslate)) {
     effectivePointerInstruction = pointerToTranslateInst;
   } else if (auto arg = dyn_cast<llvm::Argument>(pointerToTranslate)) {

compiler/lib/Translations.cpp
@@ -275,7 +275,7 @@ bool alaska::shouldTranslate(llvm::Value *val) {
 
   if (auto arg = dyn_cast<Argument>(val)) {
     auto *func = arg->getParent();
-    if (func && func->getName().startswith(".omp_outlined")) {
+    if (func && func->getName().starts_with(".omp_outlined")) {
       return false;
     }
   }

compiler/passes/Alaska.cpp
@@ -63,7 +63,7 @@ class TranslationInlinePass : public llvm::PassInfoMixin<TranslationInlinePass> 
     std::vector<llvm::CallInst *> toInline;
     for (auto &F : M) {
       if (F.empty()) continue;
-      if (F.getName().startswith("alaska_")) {
+      if (F.getName().starts_with("alaska_")) {
         for (auto user : F.users()) {
           if (auto call = dyn_cast<CallInst>(user)) {
             if (call->getCalledFunction() != &F) continue;
@@ -109,8 +109,8 @@ class SimpleFunctionPass : public llvm::PassInfoMixin<SimpleFunctionPass> {
     for (auto &F : M) {
       if (F.empty()) continue;
 
-      if (F.getName().startswith(".omp_outlined")) continue;
-      if (F.getName().startswith("omp_outlined")) continue;
+      if (F.getName().starts_with(".omp_outlined")) continue;
+      if (F.getName().starts_with("omp_outlined")) continue;
 
       llvm::DominatorTree DT(F);
       llvm::PostDominatorTree PDT(F);
@@ -123,7 +123,7 @@ class SimpleFunctionPass : public llvm::PassInfoMixin<SimpleFunctionPass> {
           if (auto *call = dyn_cast<CallInst>(&I)) {
             auto *calledFunction = call->getCalledFunction();
             // Is it an intrinsic?
-            if (calledFunction->getName().startswith("llvm.")) continue;
+            if (calledFunction->getName().starts_with("llvm.")) continue;
 
             hasProblematicCalls = true;
           }

compiler/passes/ArgumentTrace.cpp
@@ -15,9 +15,9 @@ PreservedAnalyses AlaskaArgumentTracePass::run(Module &M, ModuleAnalysisManager 
   for (auto &F : M) {
     if (F.empty()) continue;
 
-    if (F.getName().startswith("alaska.")) continue;
-    if (F.getName().startswith("alaska_")) continue;
-    if (F.getName().startswith("__alaska")) continue;
+    if (F.getName().starts_with("alaska.")) continue;
+    if (F.getName().starts_with("alaska_")) continue;
+    if (F.getName().starts_with("__alaska")) continue;
 
     IRBuilder<> atEntry(F.front().getFirstNonPHI());
 

compiler/passes/Escape.cpp
@@ -176,9 +176,9 @@ static bool mightBlock(llvm::Function &F) {
     return false;
   }
 
-  if (name.startswith("alaska.")) return false;
-  if (name.startswith("__alaska")) return false;
-  if (name.startswith("llvm.")) return false;
+  if (name.starts_with("alaska.")) return false;
+  if (name.starts_with("__alaska")) return false;
+  if (name.starts_with("llvm.")) return false;
   // alaska::println("might block: ", name);
 
   return true;
@@ -231,18 +231,18 @@ llvm::PreservedAnalyses AlaskaEscapePass::run(llvm::Module &M, llvm::ModuleAnaly
     // if the function is defined (has a body) skip it
     if (!F.empty()) continue;
     // Ignore calls to alaska functions
-    if (F.getName().startswith("alaska_")) ignore = true;
-    if (F.getName().startswith("alaska.")) ignore = true;
+    if (F.getName().starts_with("alaska_")) ignore = true;
+    if (F.getName().starts_with("alaska.")) ignore = true;
     // Intriniscs
-    if (F.getName().startswith("llvm.lifetime")) ignore = true;
-    if (F.getName().startswith("llvm.va_start")) ignore = true;
-    if (F.getName().startswith("llvm.va_end")) ignore = true;
-    if (F.getName().startswith("llvm.dbg")) ignore = true;
-    if (F.getName().startswith("llvm.smax")) ignore = true;
-    if (F.getName().startswith("llvm.smin")) ignore = true;
-    // if (F.getName().startswith("llvm.memcpy")) ignore = true;
-    // if (F.getName().startswith("llvm.memset")) ignore = true;
-    if (F.getName().startswith("llvm.experimental.noalias")) ignore = true;
+    if (F.getName().starts_with("llvm.lifetime")) ignore = true;
+    if (F.getName().starts_with("llvm.va_start")) ignore = true;
+    if (F.getName().starts_with("llvm.va_end")) ignore = true;
+    if (F.getName().starts_with("llvm.dbg")) ignore = true;
+    if (F.getName().starts_with("llvm.smax")) ignore = true;
+    if (F.getName().starts_with("llvm.smin")) ignore = true;
+    // if (F.getName().starts_with("llvm.memcpy")) ignore = true;
+    // if (F.getName().starts_with("llvm.memset")) ignore = true;
+    if (F.getName().starts_with("llvm.experimental.noalias")) ignore = true;
 
     if (ignore) functions_to_ignore.insert(std::string(F.getName()));
   }
@@ -264,29 +264,53 @@ llvm::PreservedAnalyses AlaskaEscapePass::run(llvm::Module &M, llvm::ModuleAnaly
       if (F.isVarArg() && !relax_vararg_escape()) {
         // If the function is empty, we must escape varargs
         if (F.empty()) {
+          alaska::println("ESCAPE ", F.getName(), " empty vararg to function");
           info.escape_varargs = true;
         } else {
           // Otherwise, we need to do some snooping, as a va_list could escape somewhere else.
           // The way we do this is *dumb*. If a function which has varargs calls "va_start", we
           // conservativley say that the arguments will escape somewhere else. *va_args suck*
-          if (va_start_func != NULL) {
-            for (auto user : va_start_func->users()) {
-              if (auto inst = dyn_cast<llvm::Instruction>(user)) {
-                if (inst->getFunction() == &F) {
-                  info.escape_varargs = true;
-                  break;
+
+
+          {
+            bool found = false;
+            for (auto &BB : F) {
+              for (auto &I : BB) {
+                if (auto *call = dyn_cast<CallBase>(&I)) {
+                  if (auto *callee = call->getCalledFunction()) {
+                    if (callee->getName().starts_with("llvm.va_start")) {
+                      alaska::println("ESCAPE ", F.getName(), " vararg due to va_start");
+                      info.escape_varargs = true;
+                      found = true;
+                      break;
+                    }
+                  }
                 }
               }
+              if (found) break;
             }
           }
+
+          // if (va_start_func != NULL) {
+          //   for (auto user : va_start_func->users()) {
+          //     if (auto inst = dyn_cast<llvm::Instruction>(user)) {
+          //       if (inst->getFunction() == &F) {
+          //         info.escape_varargs = true;
+          //         break;
+          //       }
+          //     }
+          //   }
+          // }
         }
       }
 
       for (auto &arg : F.args()) {
         int no = arg.getArgNo();
         if (F.empty()) {
+          alaska::println("ESCAPE ", F.getName(), " arg ", no);
           info.args.insert(no);
         } else if (arg.getType()->isPointerTy() && arg.hasAttribute(Attribute::ByVal)) {
+          alaska::println("ESCAPE ", F.getName(), " byval arg ", no);
           info.args.insert(no);
         }
       }
@@ -360,7 +384,8 @@ llvm::PreservedAnalyses AlaskaEscapePass::run(llvm::Module &M, llvm::ModuleAnaly
 
 
           if (auto func = dyn_cast<llvm::Function>(call->getCalledOperand())) {
-            // llvm::errs() << "escape to argument " << i << " at call to \e[31m\"" <<
+            // llvm::errs() << "ESCAPE " << func->getName() << " :: " << *func->getFunctionType() <<
+            // "\n"; llvm::errs() << "escape to argument " << i << " at call to \e[31m\"" <<
             // func->getName() << "\e[0m"; if (llvm::DILocation *Loc = I.getDebugLoc()) {
             //   unsigned Line = Loc->getLine();
             //   unsigned Column = Loc->getColumn();  // Optional: Get column number

compiler/passes/Lower.cpp
@@ -95,8 +95,6 @@ bool collectOffsets(GetElementPtrInst *gep, const DataLayout &DL, unsigned BitWi
 llvm::PreservedAnalyses AlaskaLowerPass::run(llvm::Module &M, llvm::ModuleAnalysisManager &AM) {
   std::set<llvm::Instruction *> to_delete;
 
-  llvm::DataLayout DL(&M);
-
   // Lower GC rooting.
   if (auto func = M.getFunction("alaska.safepoint_poll")) {
     auto pollFunc = M.getOrInsertFunction("alaska_safepoint", func->getFunctionType());
@@ -131,7 +129,7 @@ llvm::PreservedAnalyses AlaskaLowerPass::run(llvm::Module &M, llvm::ModuleAnalys
       // If the return value is only used in calls, run the more expensive "escape" variant
       bool onlyCalls = true;
       for (auto *user : call->users()) {
-        if (auto c = dyn_cast<CallInst>(user)) {
+        if (isa<CallInst>(user)) {
           // Good!
         } else {
           // onlyCalls = false;

compiler/passes/PinTracking.cpp
@@ -121,7 +121,7 @@ PreservedAnalyses PinTrackingPass::run(Module &M, ModuleAnalysisManager &AM) {
             if (not targetFuncType->getReturnType()->isVoidTy()) continue;
           }
           if (auto func = call->getCalledFunction(); func != nullptr) {
-            if (func->getName().startswith("alaska.")) continue;
+            if (func->getName().starts_with("alaska.")) continue;
             if (func->getName() == "alaska_do_handle_fault_check") continue;
           }
           statepointCalls.insert(call);
@@ -136,7 +136,7 @@ PreservedAnalyses PinTrackingPass::run(Module &M, ModuleAnalysisManager &AM) {
             if (not targetFuncType->getReturnType()->isVoidTy()) continue;
           }
           if (auto func = invoke->getCalledFunction(); func != nullptr) {
-            if (func->getName().startswith("alaska.")) continue;
+            if (func->getName().starts_with("alaska.")) continue;
           }
           statepointCalls.insert(invoke);
         }

compiler/passes/PlaceSafepoints.cpp
@@ -380,7 +380,7 @@ PreservedAnalyses PlaceSafepointsPass::run(Module &M, ModuleAnalysisManager &AM)
 static bool needsStatepoint(CallBase *Call, const TargetLibraryInfo &TLI) {
   if (auto *CI = dyn_cast<CallInst>(Call)) {
     if (auto *f = CI->getCalledFunction()) {
-      if (f->getName().startswith("alaska.")) return false;
+      if (f->getName().starts_with("alaska.")) return false;
     }
     if (CI->isInlineAsm()) return false;
   }
@@ -496,7 +496,7 @@ static bool doesNotRequireEntrySafepointBefore(CallBase *Call) {
     switch (II->getIntrinsicID()) {
       case Intrinsic::experimental_gc_statepoint:
       case Intrinsic::experimental_patchpoint_void:
-      case Intrinsic::experimental_patchpoint_i64:
+      case Intrinsic::experimental_patchpoint:
         // The can wrap an actual call which may grow the stack by an unbounded
         // amount or run forever.
         return false;
@@ -563,9 +563,7 @@ static Instruction *findLocationForEntrySafepoint(Function &F, DominatorTree &DT
 // const char GCSafepointPollName[] = "gc.safepoint_poll";
 const char GCSafepointPollName[] = "alaska_safepoint";
 
-static bool isGCSafepointPoll(Function &F) {
-  return F.getName().equals(GCSafepointPollName);
-}
+static bool isGCSafepointPoll(Function &F) { return F.getName() == GCSafepointPollName; }
 
 /// Returns true if this function should be rewritten to include safepoint
 /// polls and parseable call sites.  The main point of this function is to be
@@ -573,7 +571,7 @@ static bool isGCSafepointPoll(Function &F) {
 static bool shouldRewriteFunction(Function &F) {
   // TODO: do this w/ the gc interface instead!
   // TODO: This should check the GCStrategy
-  if (F.getName().startswith("alaska")) {
+  if (F.getName().starts_with("alaska")) {
     return false;
   }
 
@@ -596,16 +594,12 @@ static bool shouldRewriteFunction(Function &F) {
 // TODO: These should become properties of the GCStrategy, possibly with
 // command line overrides.
 static bool enableEntrySafepoints(Function &F) {
-  if (F.getName().startswith(".omp_outlined")) return true;
-  if (F.getName().startswith("omp_outlined")) return true;
+  if (F.getName().starts_with(".omp_outlined")) return true;
+  if (F.getName().starts_with("omp_outlined")) return true;
   return !NoEntry;
 }
-static bool enableBackedgeSafepoints(Function &F) {
-  return !NoBackedge;
-}
-static bool enableCallSafepoints(Function &F) {
-  return !NoCall;
-}
+static bool enableBackedgeSafepoints(Function &F) { return !NoBackedge; }
+static bool enableCallSafepoints(Function &F) { return !NoCall; }
 
 // Insert a safepoint poll immediately before the given instruction.  Does
 // not handle the parsability of state at the runtime call, that's the

compiler/passes/TranslatePass.cpp
@@ -52,7 +52,7 @@ PreservedAnalyses AlaskaTranslatePass::run(Module &M, ModuleAnalysisManager &AM)
     if (F.empty()) continue;
     auto section = F.getSection();
     // Skip functions which are annotated with `alaska_rt`
-    if (section.startswith("$__ALASKA__")) {
+    if (section.starts_with("$__ALASKA__")) {
       F.setSection("");
       continue;
     }

flake.nix
@@ -2,7 +2,7 @@
   description = "The Alaska Handle-Based Memory Management System";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -14,11 +14,11 @@
 
 
           runInputs = with pkgs; [
-            llvmPackages_16.libllvm
-            llvmPackages_16.clang
-            llvmPackages_16.stdenv
-            llvmPackages_16.libunwind
-            llvmPackages_16.openmp
+            llvmPackages_21.libllvm
+            llvmPackages_21.clang
+            llvmPackages_21.stdenv
+            llvmPackages_21.libunwind
+            llvmPackages_21.openmp
 
             (gllvm.overrideAttrs {
               doCheck = false;
@@ -59,6 +59,7 @@
             hardeningDisable = ["all"];
 
             shellHook = ''
+              unset NIX_ENFORCE_NO_NATIVE
               source $PWD/enable
             '';
           };

runtime/CMakeLists.txt
@@ -18,6 +18,12 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -gdwarf-4 ${WARNINGS} -fno-plt")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -std=c++2a -Wsign-compare -Wno-deprecated -fno-exceptions -Wno-ctad-maybe-unsupported -Wno-reorder -fno-rtti -Wno-invalid-offsetof ")
 
 
+# if clang
+if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-vla-cxx-extension -Wno-error=vla-cxx-extension ")
+endif()
+
+
 
 set(CMAKE_C_FLAGS_DEBUG "-O0")
 set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")